c1fb8800ddcd58c56ae84f7339f052ce4761fe29
Victor Alves Bugueno <victoralvesbugueno@gmail.com>
2018-03-21 23:51:13 -0300

    Testing Brain.js

---
+++ P5 Projects/Testing BrainJS/sketch.js
@@ -0,0 +1,21 @@
+function setup()
+{
+	createCanvas(800, 600);
+}
+
+function draw()
+{
+	background(0);
+
+	//create a simple feed forward neural network with backpropagation
+	var net = new brain.NeuralNetwork();
+	 
+	net.train([{input: [0, 0], output: [0]},
+	           {input: [0, 1], output: [1]},
+	           {input: [1, 0], output: [1]},
+	           {input: [1, 1], output: [0]}]);
+	 
+	var output = net.run([1, 0]);  // [0.987]
+	
+	console.log(output);
+}
